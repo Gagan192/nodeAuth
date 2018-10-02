5c3a0b1860eb9abbd3d458aab3644382f08605ea
Gagandeep <gagangoyal192@gmail.com>
2018-10-02 13:57:54 +0530

    added some admin controls

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ChatApp');
+// mongoose.connect('mongodb://localhost:27017/ChatApp');
+mongoose.connect('mongodb://gagangoyal192@gmail.com:gagan192@ds221003.mlab.com:21003/chat_app');
 
 module.exports={mongoose};
--- routes/problem.js
+++ routes/problem.js
@@ -4,7 +4,13 @@ var Problem = require('../models/problem');
 
 /* GET problems listing. */
 router.get('/', ensureAuthenticated ,function(req, res, next) {
-  res.render('problem',{title:'Discussed'});
+  Problem.find({active:true},{_id:1,problem:1,discussed:1},function(err,problem){
+    if(err){
+      res.render('login', { title: 'Login' , error:"Unable To fetch the ProBlems"});
+    }else {
+      res.render('problem',{title:'Discussed',problem:problem});
+    }
+  });
 });
 
 function ensureAuthenticated(req,res,next){
@@ -27,11 +33,65 @@ router.post('/',function(req,res,next){
       }
     else
       {
-        res.render('problem',{title:'Discussed'});
+        res.redirect('/problem');
       }
   });
 });
 
+router.get('/admin',function(req, res, next) {
+  Problem.find({},function(err,problem){
+    if(err){
+      res.render('login', { title: 'Login' , error:"Unable To fetch the ProBlems"});
+    }else {
+      res.render('problem',{title:'Discussed11',problem:problem});
+    }
+  });
+});
 
+router.get('/admin/:id',function(req, res, next) {
+  var id= req.params.id;
+  Problem.findOneAndUpdate({
+    _id:id,
+    },
+    {$set: {active:true}},{new: true}).then((todo)=>{
+      if(!todo){
+        return res.redirect('/users/login');
+      }
+      else {
+        return res.redirect('/');
+      }
+    });
+});
+
+router.get('/admin/add/:id',function(req, res, next) {
+  var id= req.params.id;
+  var problem= new Problem({
+    authId:'5baf808248962361086a445b',
+    problem:id,
+    date: new Date
+  });
+  problem.save(function(err){
+    if(err)
+      console.log(err);
+
+      res.redirect('/problem/admin');
+
+    });
+});
+
+router.get('/admin/done/:id',function(req, res, next) {
+  var id= req.params.id;
+  Problem.findOneAndUpdate({
+    _id:id,
+    },
+    {$set: {discussed:true}},{new: true}).then((todo)=>{
+      if(!todo){
+        return res.redirect('/users/login');
+      }
+      else {
+        return res.redirect('/');
+      }
+    });
+});
 
 module.exports = router;
