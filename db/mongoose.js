39bbc71d37dcf8a3b3b51f9214ae12e3869cc39a
Gagandeep <gagangoyal192@gmail.com>
2018-10-02 14:19:31 +0530

    Db username set

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/ChatApp');
-mongoose.connect('mongodb://gagangoyal192:gagan192@ds221003.mlab.com:21003/chat_app');
+mongoose.connect('mongodb://gagan192:gagan192@ds221003.mlab.com:21003/chat_app');
 
 module.exports={mongoose};
